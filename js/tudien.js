8ae77e8422896c776df2354abf6926d4da7790a0
anhtu6 <anhtu632@gmail.com>
2020-02-26 07:59:20 +0700

    tu dien don gian

---
+++ js/tudien.js
@@ -0,0 +1,28 @@
+let english = ["hello", "house", "water", "cake"];
+let vietnamese = ["xin chao", "ngoi nha", "nuoc uong", "banh ngot"];
+
+console.log(keyword);
+console.log(keyword == english[1]);
+
+
+function show() {
+    let keyword = document.getElementById('search').value;
+    let count = 0;
+    for (i = 0; i < english.length; i++) {
+        if (keyword == english[i]) {
+            document.getElementById('p1').innerHTML = vietnamese[i];
+            count =1;
+            break;
+        }
+
+
+    }
+    if (count==0) {
+        document.getElementById('p1').innerHTML = "khong tim thay";
+    }
+}
+
+function reset() {
+    document.getElementById('search').value = '';
+    document.getElementById('p1').innerHTML = '';
+}
